45e09e9eeb56bf40c5dbce1de3bb5dae433e1384
Joshua Wise <joshua@joshuawise.com>
2014-03-08 21:42:04 -0800

    Switch to animation.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,28 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static Animation *transition_anim;
+
+static void handle_anim(Animation *anim, const uint32_t animtime /* between 0 and 65536 */) {
+  static char s[24];
+  time_t tt;
+  struct tm *tm;
+  
+  time(&tt);
+  tm = localtime(&tt);
+  
+  snprintf(s, sizeof(s), "%02d:%02d:%02d %04x", tm->tm_hour, tm->tm_min, tm->tm_sec, (unsigned int)animtime);
+  text_layer_set_text(text_layer, s);
+}
+
+static AnimationImplementation anim_impl = {
+  .setup = NULL,
+  .update = handle_anim,
+  .teardown = NULL,
+};
 
 static void handle_tick(struct tm *tm, TimeUnits units_changed) {
-  static char s[24];
-  
-  snprintf(s, sizeof(s), "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
-  text_layer_set_text(text_layer, s);
+  animation_schedule(transition_anim);
 }
 
 static void window_load(Window *window) {
@@ -23,12 +39,17 @@ static void window_load(Window *window) {
   
   tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
   
+  transition_anim = animation_create();
+  animation_set_duration(transition_anim, 250);
+  animation_set_implementation(transition_anim, &anim_impl);
+  
   time(&tt);
   handle_tick(localtime(&tt), SECOND_UNIT);
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
+  animation_destroy(transition_anim);
   tick_timer_service_unsubscribe();
 }
 
