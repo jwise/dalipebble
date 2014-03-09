cda8589d6b6f211f1f839a2392528d19603251c0
Joshua Wise <joshua@joshuawise.com>
2014-03-08 22:23:44 -0800

    Now marshal everything through a layer, so I can get a GContext.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,49 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
-static Animation *transition_anim;
+#include "numbers.h"
 
-static void handle_anim(Animation *anim, const uint32_t animtime /* between 0 and 65536 */) {
-  static char s[24];
+/* Just so we all have this straight, I have:
+ *   a window load handler, that schedules
+ *   a timer handler, that schedules an animation, which
+ *   periodically invokes an animation handler, which marks a layer as dirty, which
+ *   eventually causes the layer to be updated.
+ *
+ * Four layers of callbacks.
+ *
+ * Don't you love PebbleOS?
+ */
+
+static Window *window;
+static Animation *transition_anim;
+static uint32_t animtime;
+
+static void update_layer(struct Layer *layer, GContext *ctx) {
+  char s[24];
   time_t tt;
   struct tm *tm;
+  GRect bbox = layer_get_bounds(layer);
   
   time(&tt);
   tm = localtime(&tt);
   
   snprintf(s, sizeof(s), "%02d:%02d:%02d %04x", tm->tm_hour, tm->tm_min, tm->tm_sec, (unsigned int)animtime);
-  text_layer_set_text(text_layer, s);
+  graphics_context_set_text_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx,
+                     (GRect) { .origin = { 0, 72 }, .size = { bbox.size.w, 20 } },
+                     0, GCornerNone);
+  graphics_draw_text(ctx, s, 
+                     fonts_get_system_font(FONT_KEY_GOTHIC_14),
+                     (GRect) { .origin = { 0, 72 }, .size = { bbox.size.w, 20 } },
+                     GTextOverflowModeFill,
+                     GTextAlignmentCenter,
+                     NULL
+                     );
+}
+
+static void handle_anim(Animation *anim, const uint32_t _animtime /* between 0 and 65536 */) {
+  animtime = _animtime;
+  layer_mark_dirty(window_get_root_layer(window));
 }
 
 static AnimationImplementation anim_impl = {
@@ -28,27 +58,18 @@ static void handle_tick(struct tm *tm, TimeUnits units_changed) {
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
   
-  time_t tt;
-
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Lel!");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  
+  layer_set_update_proc(window_layer, update_layer);
   tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
   
   transition_anim = animation_create();
   animation_set_duration(transition_anim, 250);
   animation_set_implementation(transition_anim, &anim_impl);
   
-  time(&tt);
-  handle_tick(localtime(&tt), SECOND_UNIT);
+  animtime = 0;
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
   animation_destroy(transition_anim);
   tick_timer_service_unsubscribe();
 }
@@ -71,7 +92,7 @@ int main(void) {
   init();
 
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
+  
   app_event_loop();
   deinit();
 }
